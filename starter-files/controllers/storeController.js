647f291e2a976c806f14fc91e9db7e04c3cbcf92
Ravi Yadav <rickcarter1806@gmail.com>
2017-07-04 08:00:26 +0530

    Start1

---
+++ starter-files/controllers/storeController.js
@@ -0,0 +1,3 @@
+exports.homePage = (req, res) => {
+    res.render('index'); //this wil look up in the views folder for index.pug file.
+};
--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
-// Do work here
-router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+const storeController = require('../controllers/storeController');
+
+router.get('/', storeController.homePage); 
+  // (req, res) => {
+  // This will look into the views folder to render file, will fetch the pug hello file
+  // We can pass info/object to our template as an optional second parameter
+  // res.render('hello', {
+  //  name: 'ravi',
+  //  dog: req.query.dog,
+  //  dog: 'snickers'
+//  });
+// });
+
+router.get('/reverse/:name', (req, res) => {
+  // Take the string, make it an array usin spread operator. Reverse the array and then join it back as a string.
+  const reverse = [...req.params.name].reverse().join(''); 
+  res.send(reverse);
 });
 
 module.exports = router;
